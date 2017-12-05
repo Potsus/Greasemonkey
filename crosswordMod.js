8bd6fbea48034f79d675db357ba6dc8b9a12184f
Alex Potts <lex.potsus@gmail.com>
2017-12-05 12:16:58 -0500

    clean up and version bump

--- crosswordMod.js
+++ crosswordMod.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Crossword Sound Mod
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Replace the crummy new nound with a more triumphant one
 // @author       Alex Potts
 // @match        *://embed.universaluclick.com/c/usat/l/*
@@ -10,6 +10,11 @@
 
 (function() {
     'use strict';
-     setTimeout(function(){window.puzzle_solve.setAttribute('src', 'http://apotts.me/mp3/tada.mp3');}, 5000);
-    // Your code here...
+
+     setTimeout(
+        function(){
+            window.puzzle_solve.setAttribute('src', 'http://apotts.me/mp3/tada.mp3');
+        }
+    , 5000);
+     
 })();
